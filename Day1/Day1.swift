7494db453773bd22487ce7dad269dd9513462118
Daniel <daniel.johns@robotsandpencils.com>
2023-11-30 21:25:20 -0800

    Day 1 Part 2

--- Day1/Day1.swift
+++ Day1/Day1.swift
@@ -9,11 +9,39 @@ import Foundation
 
 final class Day1: Day {
     func run(input: String) -> String {
-        let values = input.lines
-            .map { $0.compactMap { $0.wholeNumberValue } }
+        return input.lines
+            .map { digits(in: $0) }
             .map { Int("\($0.first!)\($0.last!)")! }
-        values.forEach { print($0) }
-        return values.sum
+            .sum
             .description
+    }
+    
+    func digits(in line: String) -> [Int] {
+        var digits: [Int] = []
+        for index in line.indices {
+            if let number = line[index].wholeNumberValue {
+                digits.append(number)
+            } else if line.range(of: "one", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(1)
+            } else if line.range(of: "two", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(2)
+            } else if line.range(of: "three", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(3)
+            } else if line.range(of: "four", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(4)
+            } else if line.range(of: "five", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(5)
+            } else if line.range(of: "six", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(6)
+            } else if line.range(of: "seven", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(7)
+            } else if line.range(of: "eight", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(8)
+            } else if line.range(of: "nine", range: index ..< line.endIndex)?.lowerBound == index {
+                digits.append(9)
+            }
+        }
+        
+        return digits
     }
 }
--- Day1/Day1Tests.swift
+++ Day1/Day1Tests.swift
@@ -13,11 +13,14 @@ final class Day1Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
 """
-        XCTAssertEqual(day.run(input: input), "142")
+        XCTAssertEqual(day.run(input: input), "281")
     }
 }

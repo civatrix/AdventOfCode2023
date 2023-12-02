c80ffb9883ab7c07196894036063caaad0d3274f
Daniel <daniel.johns@robotsandpencils.com>
2023-12-01 22:11:01 -0800

    Day 2 Part 1

--- Day2/Day2.swift
+++ Day2/Day2.swift
@@ -6,9 +6,48 @@
 //
 
 import Foundation
+import RegexBuilder
 
 final class Day2: Day {
+    let regex = Regex {
+        TryCapture(OneOrMore(.digit)) { Int($0) }
+        " "
+        Capture {
+            ChoiceOf {
+                "blue"
+                "red"
+                "green"
+            }
+        }
+    }
+    
     func run(input: String) -> String {
-        return ""
+        var matchingGames: [Int] = []
+        for line in input.lines {
+            let matches = line.matches(of: regex).map { $0.output }
+            
+            var red = 0
+            var blue = 0
+            var green = 0
+            
+            for match in matches {
+                switch match.2 {
+                case "red":
+                    red = max(match.1, red)
+                case "blue":
+                    blue = max(match.1, blue)
+                case "green":
+                    green = max(match.1, green)
+                default:
+                    fatalError("Unknown colour \(match.0)")
+                }
+            }
+            
+            if red <= 12 && green <= 13 && blue <= 14 {
+                matchingGames.append(line.allDigits[0])
+            }
+        }
+        
+        return matchingGames.sum.description
     }
 }

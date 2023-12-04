47b59c161440bbdb706e91cfe17803de3d7af005
Daniel <daniel.johns@robotsandpencils.com>
2023-12-04 09:45:14 -0800

    Day 4 Part 1

--- Day4/Day4.swift
+++ Day4/Day4.swift
@@ -9,6 +9,20 @@ import Foundation
 
 final class Day4: Day {
     func run(input: String) -> String {
-        return ""
+        let numbers = input.lines.map { $0.split(separator: ":")[1].split(separator: "|") }
+        
+        return numbers.map {
+            let lhs = Set($0[0].allDigits)
+            let rhs = Set($0[1].allDigits)
+            
+            let matches = lhs.intersection(rhs).count
+            if matches > 0 {
+                return 1 << (lhs.intersection(rhs).count - 1)
+            } else {
+                return 0
+            }
+        }
+        .sum
+        .description
     }
 }

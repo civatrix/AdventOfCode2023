a97ea17ff8ce5d0ef597a76a52453c3494bc930e
Daniel <daniel.johns@robotsandpencils.com>
2023-12-07 21:13:37 -0800

    Day 8 Part 1

--- Day8/Day8.swift
+++ Day8/Day8.swift
@@ -8,7 +8,31 @@
 import Foundation
 
 final class Day8: Day {
+    let regex = /(...) = \((...), (...)\)/
+    
     func run(input: String) -> String {
-        return ""
+        var lines = input.lines
+        let key = lines.removeFirst()
+        
+        var maps = [Substring: (Substring, Substring)]()
+        for line in lines {
+            let match = line.wholeMatch(of: regex)!.output
+            maps[match.1] = (match.2, match.3)
+        }
+        
+        var keyIndex = key.startIndex
+        var currentNode: Substring = "AAA"
+        var steps = 0
+        while currentNode != "ZZZ" {
+            let left = key[keyIndex] == "L"
+            currentNode = left ? maps[currentNode]!.0 : maps[currentNode]!.1
+            keyIndex = key.index(after: keyIndex)
+            if keyIndex == key.endIndex {
+                keyIndex = key.startIndex
+            }
+            steps += 1
+        }
+        
+        return steps.description
     }
 }
--- Day8/Day8Tests.swift
+++ Day8/Day8Tests.swift
@@ -13,7 +13,29 @@ final class Day8Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "2")
+    }
+    
+    func testDay2() throws {
+        let input =
+"""
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+"""
+        XCTAssertEqual(day.run(input: input), "6")
     }
 }
+

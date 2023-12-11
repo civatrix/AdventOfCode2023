2bcafc3be547386c034161370c137864f6de3e2a
Daniel <daniel.johns@robotsandpencils.com>
2023-12-11 08:07:08 -0800

    Day 11 Part 2

--- Day11/Day11.swift
+++ Day11/Day11.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 final class Day11: Day {
+    var expansionSize = 1_000_000
+    
     func run(input: String) -> String {
         var stars = input.parseGrid()
         
@@ -21,9 +23,9 @@ final class Day11: Day {
             
             let toModify = stars.filter { $0.x > xIndex }
             stars.subtract(toModify)
-            stars.formUnion(toModify.map { Point(x: $0.x + 1, y: $0.y) })
-            xMax += 1
-            xIndex += 2
+            stars.formUnion(toModify.map { Point(x: $0.x + expansionSize - 1, y: $0.y) })
+            xMax += expansionSize
+            xIndex += expansionSize + 1
         }
         
         var yMax = stars.map { $0.y }.max()!
@@ -36,9 +38,9 @@ final class Day11: Day {
             
             let toModify = stars.filter { $0.y > yIndex }
             stars.subtract(toModify)
-            stars.formUnion(toModify.map { Point(x: $0.x, y: $0.y + 1) })
-            yMax += 1
-            yIndex += 2
+            stars.formUnion(toModify.map { Point(x: $0.x, y: $0.y + expansionSize - 1) })
+            yMax += expansionSize
+            yIndex += expansionSize + 1
         }
         
         var sum = 0

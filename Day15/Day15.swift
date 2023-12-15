408e9cd3e04b8f2a0e9635a514fdfc50cd6cbd04
Daniel <daniel.johns@robotsandpencils.com>
2023-12-14 21:31:04 -0800

    Day 15 Part 2

--- Day15/Day15.swift
+++ Day15/Day15.swift
@@ -8,13 +8,51 @@
 import Foundation
 
 final class Day15: Day {
-    func run(input: String) -> String {
-        return input.split(separator: ",").map { hash(String($0)) }.sum.description
+    struct Lens {
+        let label: String
+        let focalLength: Int
     }
     
+    func run(input: String) -> String {
+        var boxes = [[Lens]](repeating: [], count: 256)
+        input.split(separator: ",").forEach { instruction in
+            if let match = instruction.wholeMatch(of: /(.+)=(\d)/)?.output {
+                let label = String(match.1)
+                let boxNumber = hash(label)
+                let lens = Lens(label: label, focalLength: Int(match.2)!)
+                if let existingLens = boxes[boxNumber].firstIndex(where: { $0.label == label }) {
+                    boxes[boxNumber][existingLens] = lens
+                } else {
+                    boxes[boxNumber].append(lens)
+                }
+            } else if let match = instruction.wholeMatch(of: /(.+)-/) {
+                let label = String(match.1)
+                let boxNumber = hash(label)
+                if let existingLens = boxes[boxNumber].firstIndex(where: { $0.label == label }) {
+                    boxes[boxNumber].remove(at: existingLens)
+                }
+            } else {
+                fatalError("Couldn't match instruction \(instruction)")
+            }
+        }
+        
+        return boxes.enumerated().map { (index, box) -> Int in
+            box.enumerated().map { (index + 1) * ($0.offset + 1) * $0.element.focalLength }.sum
+        }
+        .sum
+        .description
+    }
+    
+    var cache = [String: Int]()
     func hash(_ input: String) -> Int {
-        input.reduce(0) { partialResult, character in
+        if let hit = cache[input] {
+            return hit
+        }
+        
+        let result = input.reduce(0) { partialResult, character in
             ((partialResult + Int(character.asciiValue!)) * 17) % 256
         }
+        cache[input] = result
+        return result
     }
 }
--- Day15/Day15Tests.swift
+++ Day15/Day15Tests.swift
@@ -15,7 +15,7 @@ final class Day15Tests: XCTestCase {
 """
 rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
 """
-        XCTAssertEqual(day.run(input: input), "1320")
+        XCTAssertEqual(day.run(input: input), "145")
     }
     
     func testHash() {

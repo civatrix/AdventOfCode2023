9b516c2fcdc8fedb27aa82da02bf2f98f447615e
Daniel <daniel.johns@robotsandpencils.com>
2023-12-18 17:35:17 -0800

    Day 18 Part 1

--- Day18/Day18.swift
+++ Day18/Day18.swift
@@ -9,6 +9,34 @@ import Foundation
 
 final class Day18: Day {
     func run(input: String) -> String {
-        return ""
+        var current = Point.zero
+        var corners = [current]
+        var perimeter = 0
+        
+        for line in input.lines {
+            let directionString = line.first!
+            let direction: Point = switch directionString {
+            case "R": .right
+            case "L": .left
+            case "U": .up
+            case "D": .down
+            default:
+                fatalError("Invalid direction \(directionString)")
+            }
+            let distance = line.allDigits[0]
+            perimeter += distance
+            current += (direction * distance)
+            corners.append(current)
+        }
+        
+        var area = 0
+        for (p1, p2) in corners.adjacentPairs() {
+            area += (p1.x - p2.x) * (p1.y + p2.y)
+        }
+        area /= 2
+        
+        let interior = area + 1 - (perimeter / 2)
+        
+        return (perimeter + interior).description
     }
 }
